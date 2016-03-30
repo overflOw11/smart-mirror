2784560fa7210b3876aa92b97605e33a640fe6cf
Evan Cohen <evanbtcohen@gmail.com>
2016-03-30 00:53:54 -0700

    further script enhancements

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,11 @@
 # Supported versions of node: v4.x, v5.x
 NODE_VERSION="v4.*\|v5.*"
 
-# Ensure we are not using sudo (this could cause bad things to happen)
-if [ "$(whoami)" == "root" ];
+# Ensure we are using sudo
+if [ "$(whoami)" != "root" ];
 then
-	echo "Sorry, this script should not be run with 'sudo'."
-	exit 1
+	echo "This script requires root permissions, try: sudo ./${0##*/} "
+	exit 0
 fi
 
 cat << "EOF"
@@ -24,19 +24,19 @@ cat << "EOF"
 |\   _ \  _   \|\  \|\   __  \|\   __  \|\   __  \|\   __  \    
 \ \  \\\__\ \  \ \  \ \  \|\  \ \  \|\  \ \  \|\  \ \  \|\  \   
  \ \  \\|__| \  \ \  \ \   _  _\ \   _  _\ \  \\\  \ \   _  _\  
-  \ \  \    \ \  \ \  \ \  \\  \\ \  \\  \\ \  \\\  \ \  \\  \| 
+  \ \  \    \ \  \ \  \ \  \\  \\ \  \\  \\ \  \\\  \ \  \\  \  
    \ \__\    \ \__\ \__\ \__\\ _\\ \__\\ _\\ \_______\ \__\\ _\ 
     \|__|     \|__|\|__|\|__|\|__|\|__|\|__|\|_______|\|__|\|__|
 
 EOF
 
-echo "This script will install the smart-mirror and it's dependencies."
+printf "This script will install the smart-mirror and it's dependencies."
 
 # Ensure the use would like to start the install
 read -r -p "Would you like to continue? [y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    echo "Excellent! $(tput setaf 9)Please do not exit this script until it is complete.$(tput sgr0)"
+    printf "Excellent! $(tput setaf 9)Please do not exit this script until it is complete.$(tput sgr0)\n"
 else
     exit 1
 fi
@@ -49,54 +49,65 @@ then
     sed -i '/display_rotate/c\display_rotate=1' /boot/config.txt
 fi
 
-echo "Checking for node"
+printf "Checking for node..."
 node --version | grep ${NODE_VERSION}
 if [[ $? != 0 ]] ;
 then
     # Install Node
-    echo "$(tput setaf 12)Downloading Node$(tput sgr0)"
+    printf "$(tput setaf 12)Downloading Node$(tput sgr0)\n"
     wget https://nodejs.org/dist/v4.0.0/node-v4.0.0-linux-armv7l.tar.gz
     tar -xvf node-v4.0.0-linux-armv7l.tar.gz 
     cd node-v4.0.0-linux-armv7l
     
     # Copy to /usr/local
-    echo "$(tput setaf 12)Installing Node$(tput sgr0)"
+    printf "$(tput setaf 12)Installing Node$(tput sgr0)\n"
     sudo cp -R * /usr/local/
     
     # Clean up after ourselvs
     cd ..
     rm node-v4.0.0-linux-armv7l.tar.gz
     rm -R node-v4.0.0-linux-armv7l
-    echo "$(tput setaf 10)Node is now installed!$(tput sgr0)"
+    printf "$(tput setaf 10)Node is now installed!$(tput sgr0)\n"
 else
-    echo "$(tput setaf 10)node is already installed, great job!$(tput sgr0)"
+    printf "$(tput setaf 10)node is already installed, great job!$(tput sgr0)\n"
 fi
 
 # Getting the code
-echo "$(tput setaf 12)Cloning Git Repo$(tput sgr0)"
+printf "$(tput setaf 12)Cloning Git Repo$(tput sgr0)\n"
 cd ~
-git clone https://github.com/evancohen/smart-mirror.git
-echo "$(tput setaf 10)smart-mirror code is now downloaded$(tput sgr0)"
+sudo -u `logname` git clone https://github.com/evancohen/smart-mirror.git
+printf "$(tput setaf 10)smart-mirror code is now downloaded$(tput sgr0)\n"
 
 cd smart-mirror
 
-echo "$(tput setaf 12)Creating Mirror Config$(tput sgr0)"
-cp config.example.js config.js
+printf "$(tput setaf 12)Creating smart-mirror Config$(tput sgr0)\n"
+sudo -u `logname` cp config.example.js config.js
 
 # Install package to hide the mouse when inactive
-echo "$(tput setaf 12)Installing unclutter$(tput sgr0)"
+printf "$(tput setaf 12)Installing unclutter$(tput sgr0)\n"
 sudo apt-get install unclutter
 
 # Apply LXDE unclutter autostart 
 sed -i -e '$a\
 unclutter -idle 0.1 -root' /etc/xdg/lxsession/LXDE/autostart
 
-echo "$(tput setaf 12)Installing smart-mirror dependencies...$(tput sgr0)"
-echo "$(tput setaf 11)This may take a bit$(tput sgr0)"
-npm install
-echo "$(tput setaf 10)The smart-mirror is now installed!$(tput sgr0)"
+printf "$(tput setaf 12)Installing smart-mirror dependencies...$(tput sgr0)"
+printf "$(tput setaf 11)This may take a while. Go grab a beer :)$(tput sgr0)\n"
+sudo -u `logname` npm install
 
-# Launching Smart Mirror App
-npm start
+# The mirror is now installed, yay!
+cat << "EOF"
+
+        |        The smart-mirror is now installed!
+       / \       
+      / _ \      Once you fill out your config you can start the mirror with:
+     |.o '.|     npm start
+     |'._.'|     
+     |     |     To lean more, check out the documentation at:
+   ,'|  |  |`.   docs.smart-mirror.io
+  /  |  |  |  \
+  |,-'--|--'-.|
+  
+EOF
 
 exit 0
